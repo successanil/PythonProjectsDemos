26fc8e74556fe78ddf79f2a4f7b81be9a088e11f
Relsell <relsellglobal@gmail.com>
2019-09-10 21:52:49 +0530

    minor optimization

--- scrappingdemo/scraper_demo-relsellglobal.py
+++ scrappingdemo/scraper_demo-relsellglobal.py
@@ -8,14 +8,14 @@ page = requests.get('http://www.relsellglobal.in/')
 soup = BeautifulSoup(page.text, 'html.parser')
 # print(soup)
 
-# get the repo list
-section = soup.find(class_="mbr-section article mbr-section__container")
-container = section.find(class_="container")
-row = container.find(class_="row")
-desiredTag = row.find(class_="col-xs-12 lead")
-print(desiredTag)
+path_to_follow = ["mbr-section article mbr-section__container","container","row","col-xs-12 lead"]
 
-# find all instances of that class (should return 25 as shown in the github main page)
-# repo_list = repo.find_all(class_='col-12 d-block width-full py-4 border-bottom')
 
-# print(len(repo_list))
+readTag = '';
+for tag in path_to_follow:
+    # print(tag)
+    readTag = soup.find(class_=tag)
+
+print(readTag.text)
+
+
