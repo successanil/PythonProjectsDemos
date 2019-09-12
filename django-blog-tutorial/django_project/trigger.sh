928de76d06512287bb75832de2f98c46f207e57d
Ubuntu <ubuntu@ip-172-26-4-222.ap-south-1.compute.internal>
2019-09-12 02:30:20 +0000

    added admin css

--- django-blog-tutorial/django_project/trigger.sh
+++ django-blog-tutorial/django_project/trigger.sh
@@ -1,3 +1,3 @@
 #/home/ubuntu/.local/bin/gunicorn --bind localhost:8000 django_project.wsgi:application
-/usr/bin/python3.7 manage.py runserver 127.0.0.1:8011 &
-/usr/local/bin/node /home/ubuntu/nodeproj/samplenodeapp/server.js 
+/usr/bin/python3.7 manage.py runserver 127.0.0.1:8011
+#/usr/local/bin/node /home/ubuntu/nodeproj/samplenodeapp/server.js 
