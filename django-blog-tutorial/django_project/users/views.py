c274cc4f16adde8261ea591cfb701591af198723
Relsell <relsellglobal@gmail.com>
2019-09-15 07:57:39 +0530

    added user authentication for website login,register

--- django-blog-tutorial/django_project/users/views.py
+++ django-blog-tutorial/django_project/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -9,10 +10,16 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request,f'Account created for {username}')
-            return redirect('blog-home')
+            # messages.success(request,f'Account created for {username}')
+            messages.success(request, f'Your account has been created!')
+            # return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegistrationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
 
