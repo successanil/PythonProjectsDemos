a8b93b658902006b8def4541a5a1e0d77524746e
Relsell <relsellglobal@gmail.com>
2019-09-19 09:04:30 +0530

    updated code for blog tutorial

--- django-blog-tutorial/django_project/django_project/settings.py
+++ django-blog-tutorial/django_project/django_project/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static')
-MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'media')
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap'
 
