7bddc781dbe124460f9c22be8424b4e22fca9801
=successanil <success_anil@yahoo.co.in>
2019-09-20 05:31:15 +0530

    updated postgres

--- djang-docker-drf/django_rest/django_rest/settings.py
+++ djang-docker-drf/django_rest/django_rest/settings.py
@@ -86,9 +86,17 @@ WSGI_APPLICATION = 'django_rest.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drf_db',
+        'USER': 'admin',
+        'PASSWORD': 'success123',
+        'HOST': 'db',
+        'PORT': 5432                 # set to empty string for default
     }
 }
 
