f872a0099852705f09638c92aceb724a5cb4f4a1
Ubuntu <ubuntu@ip-172-26-4-222.ap-south-1.compute.internal>
2019-09-21 05:04:08 +0000

    added update

--- djang-docker-drf-nodeblog/django_rest/django_rest/settings.py
+++ djang-docker-drf-nodeblog/django_rest/django_rest/settings.py
@@ -86,9 +86,16 @@ WSGI_APPLICATION = 'django_rest.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
