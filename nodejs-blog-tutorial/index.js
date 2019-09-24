fe9faea4f76bfefc78bf509af7f9fd4f09a1b432
=successanil <success_anil@yahoo.co.in>
2019-09-24 09:44:07 +0530

    nodejs blogupdate

--- nodejs-blog-tutorial/index.js
+++ nodejs-blog-tutorial/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
 var bodyParser = require('body-parser')
-const dbUtils = require('./dbutils');
+//const dbUtils = require('./dbutils');
 
 // dbUtils.createModels();
 
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/node-blog");
+mongoose.connect("mongodb://localhost:27017/mydb?authSource=admin",{
+ "user": "admin",
+ "pass": "success123",
+ useNewUrlParser: true,
+ useUnifiedTopology: true
+ 
+}).then(()=>{
+   console.log('successfully connected');
+}).catch((err)=>{
+       console.log('Error in connection');
+});
 var postSchema = new mongoose.Schema({ body: String });
 var Post = mongoose.model('Post', postSchema);
 
@@ -46,7 +56,7 @@ app.post('/addpost', (req, res) => {
     postData.save().then( result => {
         res.redirect('/');
     }).catch(err => {
-        res.status(400).send("Unable to save data");
+        res.status(400).send("Unable to save data "+err);
     });
 });
  
